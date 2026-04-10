bc5af5b8cfb91141f8f68f5b35527b7a59c83951
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2026-04-10 10:26:13 +0000

    Updated app file with onboarding route

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,19 @@
 import "@/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-primary">
         Welcome to Nativewind!
       </Text>
+      <Link
+        href="/onboarding"
+        className="mt-4 p-4 bg-primary text-white rounded"
+      >
+        Go to Onboarding
+      </Link>
     </View>
   );
 }
