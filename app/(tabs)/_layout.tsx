ea8c290ffecf2b426ad1f45069c69cb8603a09ff
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2026-04-10 12:10:27 +0000

    feat:working on the tabs. Edited the tab item display names. removed the subsctioption details from the tabs routes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,12 @@
 import { Tabs } from "expo-router";
-import React from "react";
 
 const TabLayout = () => (
-  <Tabs>
+  <Tabs screenOptions={{ headerShown: false }}>
     <Tabs.Screen name="index" options={{ title: "Home" }} />
+    <Tabs.Screen name="insights" options={{ title: "Insights" }} />
+    <Tabs.Screen name="subscription" options={{ title: "Subscriptions" }} />
+    <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+    <Tabs.Screen name="subscriptions/[id]" options={{ href: null }} />
   </Tabs>
 );
 
