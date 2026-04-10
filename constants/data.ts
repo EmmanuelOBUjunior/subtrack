808b9aa4910d3d35c899ccf66e1373fd38fcbc62
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2026-04-10 12:18:04 +0000

    updated data.ts with the tab structure

---
+++ constants/data.ts
@@ -0,0 +1,8 @@
+import { icons } from "./icons";
+
+export const tabs = [
+  { name: "index", title: "Home", icons: icons.home },
+  { name: "insights", title: "Insights", icons: icons.activity },
+  { name: "subscription", title: "Subscriptions", icons: icons.wallet },
+  { name: "settings", title: "Settings", icons: icons.setting },
+];
