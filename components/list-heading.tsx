53540ad34f46644cc3f341cc04c13eed5f2d7e4f
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2026-04-11 18:37:41 +0000

    Created a list heading component

---
+++ components/list-heading.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const ListHeading = () => {
+  return (
+    <View>
+      <Text>ListHeading</Text>
+    </View>
+  );
+};
+
+export default ListHeading;
