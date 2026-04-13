1135ac3200ebd1f6fc84a63333d22f5f6158479f
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2026-04-13 11:45:36 +0000

    Working on the scrolling on the home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -25,39 +25,47 @@ export default function App() {
   >(null);
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <View className="home-header">
-        <View className="home-user">
-          <Image source={image.avatar} className="home-avatar" />
-          <Text className="home-user-name">{HOME_USER.name}</Text>
-        </View>
-        <Image source={icons.add} className="home-add-icon" />
-      </View>
-      <View className="home-balance-card">
-        <Text className="home-balance-label">Balance</Text>
-        <View className="home-balance-row">
-          <Text className="home-balance-amount">
-            {formatCurrency(HOME_BALANCE.amount)}
-          </Text>
-          <Text className="home-balance-date">
-            {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD/YYYY")}
-          </Text>
-        </View>
-      </View>
-      <View>
-        <ListHeading title="Upcoming" />
-        <FlatList
-          data={UPCOMING_SUBSCRIPTIONS}
-          renderItem={({ item }) => <UpcomingSubscriptionCard {...item} />}
-          keyExtractor={(item) => item.id}
-          horizontal={true}
-          ListEmptyComponent={
-            <Text className="home-empty-state">No upcoming renewals yet</Text>
-          }
-        />
-      </View>
       <View>
         <ListHeading title="All Subscriptions" />
         <FlatList
+          ListHeaderComponent={() => (
+            <>
+              <View className="home-header">
+                <View className="home-user">
+                  <Image source={image.avatar} className="home-avatar" />
+                  <Text className="home-user-name">{HOME_USER.name}</Text>
+                </View>
+                <Image source={icons.add} className="home-add-icon" />
+              </View>
+              <View className="home-balance-card">
+                <Text className="home-balance-label">Balance</Text>
+                <View className="home-balance-row">
+                  <Text className="home-balance-amount">
+                    {formatCurrency(HOME_BALANCE.amount)}
+                  </Text>
+                  <Text className="home-balance-date">
+                    {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD/YYYY")}
+                  </Text>
+                </View>
+              </View>
+              <View>
+                <ListHeading title="Upcoming" />
+                <FlatList
+                  data={UPCOMING_SUBSCRIPTIONS}
+                  renderItem={({ item }) => (
+                    <UpcomingSubscriptionCard {...item} />
+                  )}
+                  keyExtractor={(item) => item.id}
+                  horizontal={true}
+                  ListEmptyComponent={
+                    <Text className="home-empty-state">
+                      No upcoming renewals yet
+                    </Text>
+                  }
+                />
+              </View>
+            </>
+          )}
           data={HOME_SUBSCRIPTIONS}
           renderItem={({ item }) => (
             <SubscriptionCard
